93a679a34e347967f6dc1231310161a3e69ec47a
Konstantin Bartosh <rustigillo@gmail.com>
2023-06-10 15:44:20 +0600

    Fix: Login component works without useState, collecting data with hook-form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,56 +1,40 @@
 import { render } from "@testing-library/react";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function Login({ buttonText, handleLogin }) {
-  const [formValue, setFormValue] = useState({ email: "", password: "" });
-
-  const { register, formState: { errors, isValid } } = useForm({ mode: "onBlur" });
-
+  const requiredMessage = "Заполните это поле.";
+  const minLengthMessage = "Минимум 3 символа";
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i;
   const emailMinLengthMessage = "Минимум 6 символов";
   const emailMaxLenghtMessage = "Электронная почта должна содержать не более 40 символов";
   const emailPatternMessage = "Адрес электронной почты должен содержать символ ' @ ' ";
 
-  /** Обновление стейта при вводе в инпут */
-  const handleChange = (evt) => {
-    const { name, value } = evt.target;
-    setFormValue({
-      ...formValue,
-      [name]: value,
-    });
+  const {
+    register, 
+    handleSubmit, 
+    formState: { errors, isValid } 
+  } = useForm({ mode: "onChange" });
+
+  //* Отправка данных на сервер */
+  const submitData = (data) => {
+    handleLogin(data);
   };
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    handleLogin(formValue);
-    setFormValue({ email: "", password: "" });
-  };
-
-  // const onHookForm = (data) =>{
-  //   alert(JSON.stringify(data));
-  // }
-
-  // console.log(render)
   return (
     <div className="signup">
       <h2 className="signup__title">Войти</h2>
-      <form className="signup__form" name="form__login" onSubmit={handleSubmit} >
+      <form className="signup__form" name="form__login" onSubmit={handleSubmit(submitData)} >
         <input
           {...register("email", {
-            required: "Заполните это поле.",
+            required: requiredMessage,
             validate: {
               minLength: (value) => value.length > 5 || emailMinLengthMessage,
-
               maxLength: (value) => value.length <= 40 || emailMaxLenghtMessage,
-
               matchPattern: (value) => emailRegex.test(value) || emailPatternMessage,
             },
           })}
           id="email"
           type="email"
-          value={formValue.email}
-          onChange={handleChange}
           placeholder="Email"
           className="signup__input"
         />
@@ -59,17 +43,15 @@ function Login({ buttonText, handleLogin }) {
         </div>
         <input
           {...register("password", {
-            required: "Заполните это поле.",
+            required: requiredMessage,
             minLength: {
               value: 3,
-              message: "Минимум 3 символа"
+              message: minLengthMessage
             },
             maxLength: 40,
           })}          
           id="password"
           type="password"
-          value={formValue.password}
-          onChange={handleChange}
           placeholder="Пароль"
           className="signup__input"
         />
