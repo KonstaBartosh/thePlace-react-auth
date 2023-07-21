1c77d8576637aae8db642623d0f6a8b6a835a52d
Konstantin Bartosh <rustigillo@gmail.com>
2023-07-21 12:11:08 +0700

    Change url

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -81,7 +81,7 @@ class Api {
 }
 
 export const api = new Api({
-	url: 'https://auth.nomoreparties.co',
+	url: 'https://mesto.nomoreparties.co/v1/cohort-64',
 	headers: {
 		authorization: '3fea5f60-ca24-4403-9861-1d6a72c9cf2f',
 		'Content-Type': 'application/json'
