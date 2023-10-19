594cdb23db4d04bd49f3858ddabec28312cb6382
Konstantin Bartosh <rustigillo@gmail.com>
2023-10-19 14:23:10 +0700

    Refactor: change autoComplete

--- src/components/inputs/UrlInput.js
+++ src/components/inputs/UrlInput.js
@@ -13,7 +13,7 @@ const UrlInput = ( { title, register, errors, placeholder }) => {
 				type="url"
 				placeholder={placeholder}
 				className="popup__field form__input"
-				autoComplete="off"
+				autoComplete="on"
 			/>
 			<span className="form__error-message form__error-message_active">
 				{errors?.[title] && <div>{errors?.[title]?.message}</div>}
