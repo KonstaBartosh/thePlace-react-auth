6e5b197aa98271ba317ab1673b10f3054fc99f09
Konstantin Bartosh <rustigillo@gmail.com>
2023-10-19 14:35:19 +0700

    Refactor: remove reportWebVitals

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './pages/index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,5 +12,3 @@ root.render(
         </BrowserRouter>
     </React.StrictMode>
 );
-
-reportWebVitals();
